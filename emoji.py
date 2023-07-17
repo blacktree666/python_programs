1cfe3103c2aa16c8f16894902dad8265a0945b1b
blacktree666 <139207367+blacktree666@users.noreply.github.com>
2023-07-17 20:29:25 +0530

    Add files via upload

---
+++ emoji.py
@@ -0,0 +1,15 @@
+#code to print emojis
+
+s="\U0001F600"
+h="\u2764\ufe0f"
+l="\U0001F602"
+z="\U0001F910"
+b="\U0001F612"
+print("\n")
+print("smiling",s)
+print("heart",h)
+print("laughing",l)
+print("zipped",z)
+print("boring",b) 
+print("\n")
+
