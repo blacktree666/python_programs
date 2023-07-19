44529ffd3c8586da27a44c35a61452918c5cdb90
blacktree666 <139207367+blacktree666@users.noreply.github.com>
2023-07-19 20:43:35 +0530

    Add files via upload

---
+++ b_pattern.py
@@ -0,0 +1,15 @@
+import colorama 
+from colorama import Fore 
+colorama.init(autoreset=True) 
+
+
+for row in range(7):
+    for col in range(5):
+        if col == 0 or (row == 0 or row == 3 or row == 6) and (col < 4) or col == 4 and (row == 1 or row == 2 or row == 4 or row == 5):
+            print(Fore.GREEN+"B ",end="")
+        else:
+            print(" ",end=" ")
+    print()
+
+
+
