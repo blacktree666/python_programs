0fe6f7e2a0f01dd5b8751da001eeda3e62ba7a19
blacktree666 <139207367+blacktree666@users.noreply.github.com>
2023-07-26 20:12:12 +0530

    Add files via upload

---
+++ stats.py
@@ -0,0 +1,9 @@
+import statistics as st 
+arr=[5,2,3,7,2] 
+print("\n")
+print("mean:",st.mean(arr)) 
+print("median:",st.median(arr))
+print("mode:",st.mode(arr))
+print("variance:",st.variance(arr))
+print("\n")
+
