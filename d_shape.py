6ce0d78c743381a634ab6c692277373ea440eab2
blacktree666 <139207367+blacktree666@users.noreply.github.com>
2023-07-28 20:02:16 +0530

    Add files via upload

---
+++ d_shape.py
@@ -0,0 +1,13 @@
+import colorama 
+from colorama import Fore 
+colorama.init(autoreset=True)
+
+
+for row in range(7):
+    for col in range(5):
+        if (col==0) or(col==4 and(row!=0 and row!=6)) or ((row==0 or row==6) and(col>0 and col<4)):
+            print(Fore.GREEN+"D",end=" ")
+        else:
+            print(end="  ")
+    print()
+
