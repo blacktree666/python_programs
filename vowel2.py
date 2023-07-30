a248bcaf9055db5ce401cb8ce4b36d9176b9df48
blacktree666 <139207367+blacktree666@users.noreply.github.com>
2023-07-30 22:27:16 +0530

    Add files via upload

---
+++ vowel2.py
@@ -0,0 +1,10 @@
+vowels=["a","e","i","o","u"]
+names=["sachin","dave","dhoni","jonny"]
+
+for i in names:
+    if i[-1] in vowels:
+        print(i,"ends with vowel")
+    else:
+        print(i,"not ends with vowel")
+
+
